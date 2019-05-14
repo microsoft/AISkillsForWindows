762cab7c9ad129cc0a44f276e42de72a11bb4fc3
Louis-Philippe Bourret <lobourre@microsoft.com>
2019-05-14 12:34:09 -0700

    renaming and moving projects files toa  more structured hirearchy

---
+++ samples/SentimentAnalyzerCustomSkill/cpp/FaceSentimentAnalysisApp_C++/App.cpp
@@ -0,0 +1,185 @@
+// FaceSentimentAnalysis_cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "App.h"
+
+HRESULT App::FrameArrivedHandler(IMediaFrameReader* pFrameReader, IMediaFrameArrivedEventArgs*)
+{
+    HRESULT hr = S_OK;
+    ComPtr<IMediaFrameReference> spFrameRef;
+    ComPtr<IVideoMediaFrame> spVideoMediaFrame;
+    ComPtr<IVideoFrame> spVideoFrame;
+    ComPtr<ISkillBinding> spSkillBinding;
+    ComPtr<IAsyncAction> spOp;
+    AcquireSRWLockExclusive(&m_lock);
+    CHECKHR_GOTO(pFrameReader->TryAcquireLatestFrame(&spFrameRef), cleanup);
+    if (spFrameRef == nullptr)
+    {
+        hr = S_OK; goto cleanup;
+    }
+    CHECKHR_GOTO(spFrameRef->get_VideoMediaFrame(&spVideoMediaFrame), cleanup);
+    if (spVideoMediaFrame == nullptr)
+    {
+        hr = S_OK; goto cleanup;
+    }
+    CHECKHR_GOTO(spVideoMediaFrame->GetVideoFrame(&spVideoFrame), cleanup);
+
+    CHECKHR_GOTO(m_spFaceSentimentSkillBinding->SetInputImageAsync(spVideoFrame.Get(), &spOp), cleanup);
+    CHECKHR_GOTO(AwaitAction(spOp), cleanup);
+    CHECKHR_GOTO(m_spFaceSentimentSkillBinding.As(&spSkillBinding), cleanup);
+    CHECKHR_GOTO(m_spSkill->EvaluateAsync(spSkillBinding.Get(), spOp.ReleaseAndGetAddressOf()), cleanup);
+    CHECKHR_GOTO(AwaitAction(spOp), cleanup);
+    boolean bIsFaceFound;
+    std::cout << "Frame arrived:";
+    CHECKHR_GOTO(m_spFaceSentimentSkillBinding->get_IsFaceFound(&bIsFaceFound), cleanup);
+    if (bIsFaceFound)
+    {
+        SentimentType sentiment;
+        std::string sentiments[] = {
+            "neutral ",
+            "happiness ",
+            "surprise ",
+            "sadness ",
+            "anger ",
+            "disgust",
+            "fear",
+            "contempt"
+        };
+        CHECKHR_GOTO(m_spFaceSentimentSkillBinding->get_PredominantSentiment(&sentiment), cleanup);
+        std::string outText = " Your sentiment looks like:  " + sentiments[sentiment] + "\t\t\t\t\r";
+
+        std::cout << outText;
+    }
+    else
+    {
+        std::cout << "no face\t\t\t\t\t\t\r";// << std::endl;
+    }
+
+
+cleanup:
+    ReleaseSRWLockExclusive(&m_lock);
+    if (hr != S_OK)
+    {
+        std::cout << "Failed:" << std::hex << hr;
+    }
+    return hr;
+}
+
+HRESULT App::initMediaCapture()
+{
+    HRESULT hr = S_OK;
+    ComPtr<IMediaCapture5> spMediaCaptureFS;
+    ComPtr<IAsyncAction> op;
+    ComPtr<IMapView<HSTRING, MediaFrameSource*>> fsList;
+    ComPtr<IIterable<IKeyValuePair<HSTRING, MediaFrameSource*>*>> spIterable;
+    ComPtr<IIterator<IKeyValuePair<HSTRING, MediaFrameSource*>*>> spIterator;
+    ComPtr<IKeyValuePair<HSTRING, MediaFrameSource*>>             spKeyValue;
+    CHECKHR_GOTO(ActivateInstance(HStringReference(RuntimeClass_Windows_Media_Capture_MediaCapture).Get(), &m_spMediaCapture), cleanup);
+    CHECKHR_GOTO(m_spMediaCapture->InitializeAsync(&op), cleanup);
+    CHECKHR_GOTO(AwaitAction(op), cleanup);
+
+    CHECKHR_GOTO(m_spMediaCapture.As(&spMediaCaptureFS), cleanup);
+    CHECKHR_GOTO(spMediaCaptureFS->get_FrameSources(&fsList), cleanup);
+    CHECKHR_GOTO(fsList.As(&spIterable), cleanup);
+    CHECKHR_GOTO(spIterable->First(&spIterator), cleanup);
+    boolean bHasCurrent;
+    CHECKHR_GOTO(spIterator->get_HasCurrent(&bHasCurrent), cleanup);
+    if (bHasCurrent)
+    {
+        ComPtr<IKeyValuePair<HSTRING, MediaFrameSource*>> spKeyValue;
+        ComPtr<IMediaFrameSource> spFrameSource;
+        ComPtr<IAsyncOperation<MediaFrameReader*>> spOp;
+        ComPtr<IAsyncOperation<MediaFrameReaderStartStatus>> spOp1;
+        MediaFrameReaderStartStatus Stat;
+        CHECKHR_GOTO(spIterator->get_Current(&spKeyValue), cleanup);
+        CHECKHR_GOTO(spKeyValue->get_Value(&spFrameSource), cleanup);
+        ComPtr<IMediaFrameSourceInfo> spInfo;
+        spFrameSource->get_Info(&spInfo);
+        MediaStreamType streamType;
+        MediaFrameSourceKind sourceKind;
+        spInfo->get_MediaStreamType(&streamType);
+        spInfo->get_SourceKind(&sourceKind);
+        std::cout << "FrameSourceType:" << streamType << std::endl;
+        while (((streamType != MediaStreamType::MediaStreamType_VideoPreview) && (streamType != MediaStreamType::MediaStreamType_VideoRecord)) || (sourceKind != MediaFrameSourceKind::MediaFrameSourceKind_Color))
+        {
+            boolean bHasCurrent1;
+            CHECKHR_GOTO(spIterator->MoveNext(&bHasCurrent1), cleanup);
+            if (!bHasCurrent1)
+            {
+                continue;
+            }
+            CHECKHR_GOTO(spIterator->get_Current(spKeyValue.ReleaseAndGetAddressOf()), cleanup);
+            CHECKHR_GOTO(spKeyValue->get_Value(spFrameSource.ReleaseAndGetAddressOf()), cleanup);
+            spFrameSource->get_Info(spInfo.ReleaseAndGetAddressOf());
+            spInfo->get_MediaStreamType(&streamType);
+            spInfo->get_SourceKind(&sourceKind);
+            std::cout << "FrameSourceType:" << streamType << std::endl;
+        }
+
+        CHECKHR_GOTO(spMediaCaptureFS->CreateFrameReaderAsync(spFrameSource.Get(), &spOp), cleanup);
+        CHECKHR_GOTO(AwaitTypedResult(spOp, MediaFrameReader*, m_spFrameReader), cleanup);
+
+        m_spFrameArrivedHandlerDelegate = 
+            Microsoft::WRL::Callback<ITypedEventHandler<MediaFrameReader*, MediaFrameArrivedEventArgs*>>
+            ([&](IMediaFrameReader * pFrameReader, IMediaFrameArrivedEventArgs* eventArgs)
+                {
+                    return FrameArrivedHandler(pFrameReader, eventArgs);
+                }
+            );
+
+        CHECKHR_GOTO(m_spFrameReader->add_FrameArrived(m_spFrameArrivedHandlerDelegate.Get(), &m_token), cleanup);
+        CHECKHR_GOTO(m_spFrameReader->StartAsync(&spOp1), cleanup);
+        CHECKHR_GOTO(AwaitTypedResult(spOp1, MediaFrameReaderStartStatus, Stat), cleanup);
+    }
+
+cleanup:
+    return hr;
+}
+
+HRESULT App::deInitMediaCapture()
+{
+    HRESULT hr = S_OK;
+    ComPtr<IAsyncAction> spOp;
+    CHECKHR_GOTO(m_spFrameReader->remove_FrameArrived(m_token), cleanup);
+    CHECKHR_GOTO(m_spFrameReader->StopAsync(&spOp), cleanup);
+    CHECKHR_GOTO(AwaitAction(spOp), cleanup);
+    m_spFrameReader = nullptr;
+    m_spMediaCapture = nullptr;
+    
+cleanup:
+    return hr;
+
+}
+
+int App::AppMain()
+{
+    HRESULT hr;
+    ComPtr<ISkillDescriptor> spSkillDesc;
+    ComPtr<IAsyncOperation<ISkill*> > spOp;
+    ComPtr<ISkillBinding> spSkillBinding;
+    ComPtr<IActivationFactory> spFactory;
+    ComPtr<IAsyncOperation<ISkillBinding*>> spOp1;
+    std::cout << "Face it!\n";
+
+    RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+    CHECKHR_GOTO(initialize, cleanup);
+    CHECKHR_GOTO(ActivateInstance(HStringReference(RuntimeClass_Contoso_FaceSentimentAnalyzer_FaceSentimentAnalyzerDescriptor).Get(), &spSkillDesc), cleanup);
+    CHECKHR_GOTO(spSkillDesc->CreateSkillAsync(&spOp), cleanup);
+    CHECKHR_GOTO(Await(spOp, m_spSkill), cleanup);
+    {
+        CHECKHR_GOTO(m_spSkill->CreateSkillBindingAsync(&spOp1), cleanup);
+        CHECKHR_GOTO(Await(spOp1, spSkillBinding), cleanup);
+        CHECKHR_GOTO(spSkillBinding.As(&m_spFaceSentimentSkillBinding), cleanup);
+    }
+
+    CHECKHR_GOTO(initMediaCapture(), cleanup);
+    std::cout << "\t\t\t\t\t\t\t\t...press enter to Stop" << std::endl;
+    
+    //wait for enter keypress
+    while (std::cin.get() != '\n');
+
+    CHECKHR_GOTO(deInitMediaCapture(), cleanup);
+
+cleanup:
+    return hr;
+}
+
---
+++ samples/SentimentAnalyzerCustomSkill/cpp/FaceSentimentAnalysisApp_C++WinRT/App.cpp
@@ -0,0 +1,135 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+#include "App.h"
+
+// Function to handle the frame when it arrives from FrameReader
+winrt::Windows::Foundation::IAsyncAction App::FrameArrivedHandler(MediaFrameReader FrameReader, MediaFrameArrivedEventArgs)
+{
+    // Lock context so multiple overlapping events from FrameReader do not race for the resources.
+    m_lock.lock();
+    MediaFrameReference mediaFrame(nullptr);
+
+    // Try to get the actual Video Frame from the FrameReader
+    mediaFrame = FrameReader.TryAcquireLatestFrame();
+    if (mediaFrame != nullptr)
+    {
+        auto vmFrame = mediaFrame.VideoMediaFrame();
+        if (vmFrame != nullptr)
+        {
+            auto videoFrame = vmFrame.GetVideoFrame();
+
+            // Set the video frame on the skill binding.
+            m_FaceSentimentSkillBinding.SetInputImageAsync(videoFrame).get();
+
+            // Evaluate sentiments in video frame using the skill
+            m_Skill.EvaluateAsync(m_FaceSentimentSkillBinding).get();
+            if (m_FaceSentimentSkillBinding.IsFaceFound())
+            {
+                auto sentiment = m_FaceSentimentSkillBinding.PredominantSentiment();
+                static const std::string sentiments[] = {
+                    "neutral ",
+                    "happiness ",
+                    "surprise ",
+                    "sadness ",
+                    "anger ",
+                    "disgust",
+                    "fear",
+                    "contempt"
+                };
+                std::string outText = "\tYour sentiment looks like:  " + sentiments[(int)sentiment] + "\t\t\t\t\r";
+                std::cout << outText;
+            }
+            else
+            {
+                std::cout << "\tNo face detected\t\t\t\t\t\t\r";
+            }
+            videoFrame.Close();
+        }
+        mediaFrame.Close();
+    }
+    
+    m_lock.unlock();
+    co_return;
+}
+
+void App::InitCameraAndFrameSource()
+{
+    // Initialize media capture with default settings
+    m_mediaCapture = winrt::Windows::Media::Capture::MediaCapture();
+    m_mediaCapture.InitializeAsync().get();
+
+    // Get a list of available Frame source and iterate through them to find a Video preview/record source with Color images ( and not IR/depth etc)
+    auto fsIter = m_mediaCapture.FrameSources().First();
+    do
+    {
+        auto mediaStreamType = fsIter.Current().Value().Info().MediaStreamType();
+        auto sourceKind = fsIter.Current().Value().Info().SourceKind();
+        std::cout << "MediaStreamType:" << (int)mediaStreamType << " MediaFrameSourceKind:" << (int)sourceKind << std::endl;
+        if (
+            ((mediaStreamType == MediaStreamType::VideoPreview)
+                || (mediaStreamType == MediaStreamType::VideoRecord))
+            && (sourceKind == MediaFrameSourceKind::Color))
+        {
+            break;
+        }
+    } while (fsIter.MoveNext());
+    if (!fsIter.HasCurrent())
+    {
+        std::cout << "No valid video frame sources were found with source type color.";
+        throw new winrt::hresult(MF_E_INVALIDMEDIATYPE);
+    }
+
+    // Create FrameReader with the FrameSource that we selected in the loop above.
+    m_frameReader = m_mediaCapture.CreateFrameReaderAsync(fsIter.Current().Value()).get();
+
+    // Set up a delegate to handle the frames when they are ready
+    m_frameReader.FrameArrived({ this, &App::FrameArrivedHandler });
+
+    // Finally start the FrameReader
+    m_frameReader.StartAsync().get();
+}
+
+void App::DeInitCameraAndFrameSource()
+{
+    //Stop FrameReader and close
+    m_frameReader.StopAsync().get();
+    m_frameReader.Close();
+    m_frameReader = nullptr;
+    m_mediaCapture.Close();
+    m_mediaCapture = nullptr;
+}
+
+int App::AppMain()
+{
+    std::cout << "WinrtCPP Non-packaged(win32) console APP: Face it!!" << std::endl;
+
+    try 
+    {
+        // Create the FaceSentimentAnalyzer skill descriptor
+        auto skillDesc = FaceSentimentAnalyzerDescriptor().as<ISkillDescriptor>();
+
+        // Create instance of the skill
+        m_Skill = skillDesc.CreateSkillAsync().get().as<FaceSentimentAnalyzerSkill>();
+
+        // Create instance of the skill binding
+        m_FaceSentimentSkillBinding = m_Skill.CreateSkillBindingAsync().get().as<FaceSentimentAnalyzerBinding>();
+
+        // Initialize MediaCapture and FrameReader
+        InitCameraAndFrameSource();
+        std::cout << "\t\t\t\t\t\t\t\t...press enter to Stop" << std::endl;
+
+        // Wait for enter keypress
+        while (std::cin.get() != '\n');
+
+        std::cout << std::endl << "Key pressed.. exiting";
+
+        // De-initialize the MediaCapture and FrameReader
+        DeInitCameraAndFrameSource();
+    }
+    catch (hresult_error const &ex)
+    {
+        std::wcout << "Error:" << ex.message().c_str() << ":" << std::hex << ex.code().value << std::endl;
+        return ex.code().value;
+    }
+    return 0;
+}
+
