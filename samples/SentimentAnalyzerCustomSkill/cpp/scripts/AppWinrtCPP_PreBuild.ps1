1ed34d4a58385dfe9518ecfc44e5738b3147c3f3
louis-philippeBourret <lpbourret@outlook.com>
2019-05-16 17:00:11 -0400

    - modify scripts: remove unused param, leverage more macros from VS to point to correct directories, add description headers - modify WRL and C++/WinRT proj pre and post build commands to avoid execution policy failure and add debug comment, add missing .lib - modify Readme.MD to take into account new projects and required steps - rev-up nuspec

--- samples/SentimentAnalyzerCustomSkill/cpp/scripts/AppWinrtCPP_PreBuild.ps1
+++ samples/SentimentAnalyzerCustomSkill/cpp/scripts/AppWinrtCPP_PreBuild.ps1
@@ -1,4 +1,16 @@
-param($OutDir,$ProjectDir,$WindowsSDK_UnionMetadataPath)
+<#
+.SYNOPSIS
+Generates headers (.h) files from a set of referenced .winmd files.
+#>
+<#
+.DESCRIPTION
+-OutDir: output folder path to place the generated header (.h) files
+-ProjectDir: path to the folder of the app project that contains package.config detailing dependencies
+-PackageDir: path to the folder containing the dependency packages pulled from NuGet
+-WindowsSDK_UnionMetadataPath: folder path of the SDK union metadata that contains Windows.winmd (i.e.: C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.18362.0)
+#>
+
+param($OutDir,$ProjectDir,$PackageDir,$WindowsSDK_UnionMetadataPath)
 
 [xml]$packages = Get-Content "$ProjectDir\packages.config"
 #echo $packages.packages
@@ -6,7 +18,7 @@ $winmdFiles = @()
 
 foreach ($package in $packages.packages.package)
 {
-    $searchpath = "$ProjectDir" + "..\packages\" + $package.id + "." + $package.version
+    $searchpath = $PackageDir + "\packages\" + $package.id + "." + $package.version
     $winmdFiles += Get-ChildItem -Path "$searchpath" -Recurse -Filter *.winmd | %{$_.FullName}
 }
 $OutDir = "$ProjectDir" + "inc"
@@ -22,6 +34,7 @@ foreach($file in $winmdFiles)
 $winmdFiles = Get-ChildItem -Path "$OutDir" -Filter *.winmd | %{$_.FullName}
 foreach($file in $winmdFiles)
 {
+	echo "Generating headers for $file"
     $winrtHeaderScript =  "cppwinrt -in " + "'$file'" + " -ref " + "'$OutDir'" +  " -ref " + "'$WindowsSDK_UnionMetadataPath'" + " -out "+ "'$OutDir'" 
     Invoke-Expression "$winrtHeaderScript"
 }
