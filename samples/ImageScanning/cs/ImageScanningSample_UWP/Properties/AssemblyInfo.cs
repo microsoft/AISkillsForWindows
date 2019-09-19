afd595171f4d49695d04f8d37a8efad401054fc6
louis-philippeBourret <lpbourret@outlook.com>
2019-09-18 22:16:42 -0400

    addressing assemblyInfo mismatch for uwp app

--- samples/ImageScanning/cs/ImageScanningSample_UWP/Properties/AssemblyInfo.cs
+++ samples/ImageScanning/cs/ImageScanningSample_UWP/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConceptTaggerSample")]
+[assembly: AssemblyTitle("ImageScanningSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConceptTaggerSample")]
+[assembly: AssemblyProduct("ImageScanningSample")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
